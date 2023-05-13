3a4752b27d2bcc72ea9769210164ae0cba24b6ed
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-05-13 22:48:13 +0530

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import logo from "./logo.png";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <Routes>
+        <Route path="/" />
+        <Route path="/products" />
+      </Routes>
     </div>
   );
 }
