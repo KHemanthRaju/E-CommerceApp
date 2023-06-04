f8f9b3b0b1e0770f9f18d400e6ef0303f53f1720
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-06-04 14:47:57 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
 export { cartReducer } from "./CartReducer";
 export { filterReducer } from "./filterReducer";
 export { WishlistReducer } from "./wishlistReducer";
-export { defaultFilterState } from "../reducers/DefaultFilterState.jsx";
+export { defaultFilterState } from "./DefaultFilterState";
