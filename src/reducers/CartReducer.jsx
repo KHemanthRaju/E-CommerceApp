93a84e1ed9a4717d821e73518cda3aa9150ef7c9
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-06-04 14:33:34 +0530

    Update CartReducer.jsx

--- src/reducers/CartReducer.jsx
+++ src/reducers/CartReducer.jsx
@@ -1,4 +1,4 @@
-const cartReducer = (state, action) => {
+export const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
       const product = action.payload;
@@ -62,5 +62,3 @@ const cartReducer = (state, action) => {
       return state;
   }
 };
-
-export { cartReducer };
